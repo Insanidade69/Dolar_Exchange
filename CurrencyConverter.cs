2ef947d3bbcea28802f5c327dc4752210a5c64f9
Marcelo <mvinicius.louza@gmail.com>
2022-01-11 10:08:30 -0300

    Adicionar arquivos de projeto.

---
+++ CurrencyConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Dolar_Exchange
+{
+    internal class CurrencyConverter
+    {
+        public static double Iof = 6.0;
+
+        public static double BuyDolar(double amount, double quote)
+        {
+            double total = amount * quote;
+            return total + total * Iof / 100.0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Dolar_Exchange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the dolar exchange rate?");
+            Console.Write("Quote US$ ");
+            double quote = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("What amount u want buy?");
+            Console.Write("US$ ");
+            double amount = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double result = CurrencyConverter.BuyDolar(quote, amount);
+
+            Console.WriteLine("Value to pay R$ {0}", result.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
